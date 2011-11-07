5a9da573f80b3a4d212c38c01279fd48f4126a48
g_1 <naka.takahiro@gmail.com>
2011-11-07 17:19:17 +0900

    First Commit

---
+++ TimeTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TimeTable
+//
+//  Created by Naka Takahiro on 11/11/07.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
